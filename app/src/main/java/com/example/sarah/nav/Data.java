882262fa65f52cee8093f82ff78b4f608c193bef
SarahSolkar <sarahsolkar123@gmail.com>
2019-09-12 15:45:17 +0530

    Update Data.java

--- app/src/main/java/com/example/sarah/nav/Data.java
+++ app/src/main/java/com/example/sarah/nav/Data.java
@@ -2,32 +2,44 @@ package com.example.sarah.nav;
 
 public class Data {
 
-    private String description;
-    private String index1;
-    private String index2;
-    private String imagePath;
+//    private String description;
+//    private String index1;
+//    private String index2;
+//    private String imagePath;
+    private String restaurant_name;
+    private String category;
+    private String imgname;
+    private String price;
+    private String rid;
 
-    Data(String imagePath, String index1, String index2, String description) {
-        this.imagePath = imagePath;
-        this.description = description;
-        this.index1 = index1;
-        this.index2 = index2;
+    Data(String restaurant_name, String category, String imgname, String price, String rid) {
+        this.restaurant_name = restaurant_name;
+        this.category = category;
+        this.imgname = imgname;
+        this.price = price;
+        this.rid = rid;
     }
 
-    public String getDescription() {
-        return description;
+    public String getRestaurant_name() {
+        return restaurant_name;
     }
+    public String getCategory(){return category;}
 
-    String getImagePath() {
-        return imagePath;
-    }
+    public String getImgname() { return imgname; }
 
-    public String getIndex1() {
-        return index1;
-    }
+    public String getPrice() { return price; }
 
-    public String getIndex2() {
-        return index2;
-    }
+    public String getRid() {return rid; }
+    //    String getImagePath() {
+//        return imagePath;
+//    }
+//
+//    public String getIndex1() {
+//        return index1;
+//    }
+//
+//    public String getIndex2() {
+//        return index2;
+//    }
 
 }
