83a4a7b3b88e6af534a489afe03b5237256f0090
SarahSolkar <sarahsolkar123@gmail.com>
2019-09-12 18:28:53 +0530

    Update HomeFragment.java

--- app/src/main/java/com/example/sarah/nav/HomeFragment.java
+++ app/src/main/java/com/example/sarah/nav/HomeFragment.java
@@ -2,25 +2,20 @@ package com.example.sarah.nav;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.media.Image;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.content.SharedPreferences;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
-import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,7 +23,6 @@ import com.android.volley.RequestQueue;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.Volley;
 import com.lorentzos.flingswipe.SwipeFlingAdapterView;
-import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
 import org.json.JSONException;
@@ -38,35 +32,27 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-import static android.content.Context.MODE_PRIVATE;
 
 public class HomeFragment extends Fragment {
     private MyAppAdapter myAppAdapter;
     ArrayList<Data> array;
     SwipeFlingAdapterView flingContainer;
-    SharedPreferences pref;
-    //final String URL="http://192.168.0.103:8080/swipe";
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
         return inflater.inflate(R.layout.home,container,false);
-
     }
-
-
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-
         array = new ArrayList<>();
         getIp ip = new getIp();
         String del = ip.getIp();
+
         //array.add(new Data("http://192.168.0.103:8080/images/nonveg/1082564.jpg", "1", "64", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."));
-        array.add(new Data(""+del+":8080/images/nonveg/1082564.jpg", "1", "64", "Lorem ipsum dolor sit amet, consectetur adipiscing elit,t esse cillum dolore eu fugiat nulla pariatur.  anim id est laborum."));
+        array.add(new Data("Guddu ka Dhaaba", "apple pie", "407267", "50", "Lorem ipsum dolor sit amet, consectetur adipiscing elit,","1"));
 
         getUrls();
         Log.d("new", "initial " + array);
@@ -88,11 +74,11 @@ public class HomeFragment extends Fragment {
                 }, 2000);
             }
         });
+
         flingContainer.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
             @Override
             public void removeFirstObjectInAdapter() {
                 //myAppAdapter.notifyDataSetChanged();
-
             }
 
             @Override
@@ -110,11 +96,24 @@ public class HomeFragment extends Fragment {
 
                 Data a=array.remove(0);
                 Log.d("array remove", String.valueOf(a));
-                String index1=a.getIndex1();
-                String index2=a.getIndex2();
-//                Log.d("index1",a.getIndex1());
-//                Log.d("index1",a.getIndex2());
-                rightSwipe(index1,index2);
+
+                String restaurant_name=a.getRestaurant_name();
+                String category = a.getCategory();
+                String imgname = a.getImgname();
+                String rid = a.getRid();
+                //String desc = a.getDescription();
+
+                Log.d("right card rest name",restaurant_name);
+                Log.d("right card category ",category);
+                Log.d("right card img ",imgname);
+                Log.d("right card rid ",rid);
+                //Log.d("right card desc",desc);
+
+
+
+
+
+                rightSwipe(restaurant_name,category,imgname,rid);
 
                 myAppAdapter.notifyDataSetChanged();
                 Toast.makeText(getActivity().getApplicationContext(),"RiGHT",Toast.LENGTH_SHORT).show();
@@ -201,9 +200,11 @@ public class HomeFragment extends Fragment {
                 viewHolder = (ViewHolder) convertView.getTag();
             }
             //viewHolder.DataText.setText(parkingList.get(position).getDescription());
-            viewHolder.DataText.setText(parkingList.get(position).getDescription() + "");
+            viewHolder.DataText.setText(parkingList.get(position).getDescription() + "" +parkingList.get(position).getPrice()+""+parkingList.get(position).getRestaurant_name());
 
-            String urlImage = parkingList.get(position).getImagePath();
+            //String urlImage = parkingList.get(position).getImagePath();
+            String urlImage = parkingList.get(position).getImgname();
+
             Log.d("url","url "+urlImage);
             //Glide.with(getApplicationContext()).load(urlImage).into(viewHolder.cardImage);
             Picasso.get().load(urlImage).error(R.drawable.ic_launcher_background).fit().centerCrop().into(viewHolder.cardImage);
@@ -234,27 +235,40 @@ public class HomeFragment extends Fragment {
                 @Override
                 public void onSuccessResponse(String result) {
                     System.out.print("Bool" + result);
-                    //boolean check = sharedPref.getBoolean("abcd",false);
                     if (result != null) {
-
-                        // Toast.makeText(Login.this, result, Toast.LENGTH_SHORT).show();
-                        // textView.setText(result);
                         //String[] load = result.split(",");
-                        String ip= null, index1 = null, index2 = null, description = null;
+                        //String ip= null, index1 = null, index2 = null, description = null;
+
+                        String ip, restaurant_name = null, category = null, price = null,description = null, rid=null, imgname = null;
+
                         try {
                             JSONObject jsonObject1 = new JSONObject(result);
                             ip = jsonObject1.getString("ip");
-                            index1 = jsonObject1.getString("index1");
-                            index2 = jsonObject1.getString("index2");
+                            restaurant_name = jsonObject1.getString("restaurant_name");
+                            category = jsonObject1.getString("category");
+                            imgname = jsonObject1.getString("imgname");
+                            price = jsonObject1.getString("price");
+                            rid = jsonObject1.getString("rid");
                             description = jsonObject1.getString("description");
 
-                            System.out.println("ip :"+ip+ " index1 : "+index1 );
+                            System.out.println("ip : "+ip+" restaurant_name : "+restaurant_name+" category = "+category+" price = "+price+" rid = "+rid );
 
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
 
-                        array.add(new Data(ip,index1,index2,description));
+                        //array.add(new Data(ip,index1,index2,description));
+//                        Log.d("get url rest name",restaurant_name);
+//                        Log.d("get url category ",category);
+//                        Log.d("get url img ",imgname);
+//                        Log.d("get url rid ",rid);
+//                        Log.d("get url price ",price);
+//                        Log.d("get url desc ",description);
+
+
+                        array.add(new Data(restaurant_name,category,imgname,price,rid,description));
+
+
                         Log.d("Result", "Result is " + result +" and "+ array);
                         myAppAdapter = new MyAppAdapter(array, getActivity());
                         flingContainer.setAdapter(myAppAdapter);
@@ -277,50 +291,70 @@ public class HomeFragment extends Fragment {
 
     }//end of getUrls()
 
-    public void rightSwipe(String index1,String index2){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public void rightSwipe(String restaurant_name,String category, String imgname, String rid){
         getIp ip = new getIp();
         String del = ip.getIp();
 
         RequestQueue requestQueue = Volley.newRequestQueue(Objects.requireNonNull(getActivity()).getApplicationContext());
-        //String URL = "http://192.168.0.103:8080/addSwipe";
         String URL = ""+del+":8080/swipe";
 
         final JSONObject jsonObject = new JSONObject();
         try {
             jsonObject.put("email", "leeaanair@gmail.com");
-            jsonObject.put("index1",index1);
-            jsonObject.put("index2",index2);
+            jsonObject.put("restaurant_name",restaurant_name);
+            jsonObject.put("category",category);
+            jsonObject.put("image",imgname);
+            jsonObject.put("rid",rid);
             jsonObject.put("swipe","1");
-            //jsonObject.put("location")
+            Log.d("okay",rid);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
         final String requestBody = jsonObject.toString();
-        Log.d("str", "str is" + requestBody);
+        Log.d("swipe", "swipe str is" + requestBody);
         ConnectionManager.sendData(requestBody, requestQueue, URL, new ConnectionManager.VolleyCallback() {
             @Override
             public void onSuccessResponse(String result) {
                 System.out.print("Bool" + result);
-                //boolean check = sharedPref.getBoolean("abcd",false);
-                if (result != null) {
-                    String ip= null, index1 = null, index2 = null, description = null;
-                    try {
-                        JSONObject jsonObject1 = new JSONObject(result);
-                        ip = jsonObject1.getString("ip");
-                        index1 = jsonObject1.getString("index1");
-                        index2 = jsonObject1.getString("index2");
-                        description=jsonObject1.getString("description");
+                if (result.equals("1")) {
+                    //String ip= null, restaurant_name = null, category = null, imgname = null, rid = null,description = null;
+//                    try {
+//                        JSONObject jsonObject1 = new JSONObject(result);
+//                        ip = jsonObject1.getString("ip");
+//                        restaurant_name = jsonObject1.getString("restaurant_name");
+//                        category = jsonObject1.getString("category");
+//                        imgname = jsonObject1.getString("image");
+//                        rid = jsonObject1.getString("rid");
+//                        description = jsonObject1.getString("description");
+//
+//                        System.out.println("ip :"+ip+ "restaurant_name : "+restaurant_name+"category = "+category+"image name = "+imgname+"rid = "+rid );
+//
+//                    } catch (JSONException e) {
+//                        e.printStackTrace();
+//                    }
+//
+//                    array.add(new Data(restaurant_name,category,imgname,rid,description));
+//                    Log.d("Result", "Result is " + result +" and "+ array);
+//                    myAppAdapter = new MyAppAdapter(array, getActivity());
+//                    flingContainer.setAdapter(myAppAdapter);
+                    Toast.makeText(getActivity().getApplicationContext(), "Inserted into WishList", Toast.LENGTH_SHORT).show();
 
-                        System.out.println("ip :"+ip+ " index1 : "+index1 );
-
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-
-                    array.add(new Data(ip,index1,index2,description));
-                    Log.d("Result", "Result is " + result +" and "+ array);
-                    myAppAdapter = new MyAppAdapter(array, getActivity());
-                    flingContainer.setAdapter(myAppAdapter);
 
                 } else {
                     Toast.makeText(getActivity().getApplicationContext(), "Something went wrong", Toast.LENGTH_SHORT).show();
