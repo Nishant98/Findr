92a0ffda07888b0e7bf648a708eefab445891a38
SarahSolkar <sarahsolkar123@gmail.com>
2019-09-12 18:28:09 +0530

    Update Data.java

--- app/src/main/java/com/example/sarah/nav/Data.java
+++ app/src/main/java/com/example/sarah/nav/Data.java
@@ -2,44 +2,33 @@ package com.example.sarah.nav;
 
 public class Data {
 
-//    private String description;
-//    private String index1;
-//    private String index2;
-//    private String imagePath;
+    private String description;
     private String restaurant_name;
     private String category;
     private String imgname;
     private String price;
     private String rid;
 
-    Data(String restaurant_name, String category, String imgname, String price, String rid) {
+    Data(String restaurant_name, String category, String imgname, String price, String description,String rid) {
         this.restaurant_name = restaurant_name;
         this.category = category;
         this.imgname = imgname;
         this.price = price;
+        this.description=description;
         this.rid = rid;
     }
 
     public String getRestaurant_name() {
         return restaurant_name;
     }
+    public String getRid(){ return rid; }
+
+    public String getDescription() { return description; }
+
     public String getCategory(){return category;}
 
     public String getImgname() { return imgname; }
-
     public String getPrice() { return price; }
 
-    public String getRid() {return rid; }
-    //    String getImagePath() {
-//        return imagePath;
-//    }
-//
-//    public String getIndex1() {
-//        return index1;
-//    }
-//
-//    public String getIndex2() {
-//        return index2;
-//    }
 
 }
