87aaeb5f88e58f92222cab0948379ebbedc1f469
leeaanair <36276816+leeaanair@users.noreply.github.com>
2019-09-12 19:07:18 +0530

    Update HomeFragment.java

--- app/src/main/java/com/example/sarah/nav/HomeFragment.java
+++ app/src/main/java/com/example/sarah/nav/HomeFragment.java
@@ -52,7 +52,7 @@ public class HomeFragment extends Fragment {
         String del = ip.getIp();
 
         //array.add(new Data("http://192.168.0.103:8080/images/nonveg/1082564.jpg", "1", "64", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."));
-        array.add(new Data("Guddu ka Dhaaba", "apple pie", "407267", "50", "Lorem ipsum dolor sit amet, consectetur adipiscing elit,","1"));
+        array.add(new Data("Guddu ka Dhaaba", "apple pie", "407267.jpg", "50", "Lorem ipsum dolor sit amet, consectetur adipiscing elit,","1"));
 
         getUrls();
         Log.d("new", "initial " + array);
