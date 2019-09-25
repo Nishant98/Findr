5f6f50d02bd9f69e41ab017c8cf35a558094805b
SarahSolkar <35928732+SarahSolkar@users.noreply.github.com>
2019-09-25 22:59:24 +0530

    Update ProfileFragment.java

--- app/src/main/java/com/example/sarah/nav/ProfileFragment.java
+++ app/src/main/java/com/example/sarah/nav/ProfileFragment.java
@@ -12,39 +12,30 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class ProfileFragment extends Fragment {
+import java.util.HashMap;
 
-    Button btnLogin, btnRegister;
-    TextView name;
+public class ProfileFragment extends Fragment {
+    TextView name,email;
+    String email_session="";
+    SessionManager sessionManager;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view =  inflater.inflate(R.layout.profile, container, false);
 
-        name = view.findViewById(R.id.name);
-        name.setText("Sarah");
+        sessionManager = new SessionManager(getActivity().getApplicationContext());
+        sessionManager.checkLogin();
+        HashMap<String, String> user = sessionManager.getUserDetails();
+        email_session = user.get(sessionManager.EMAIL);
+
+//        name = view.findViewById(R.id.name);
+//        name.setText("Sarah");
+
+        email = view.findViewById(R.id.email);
+        email.setText(email_session);
 
 
         return view;
     }
 }
-//        btnRegister = view.findViewById(R.id.registerButton);
-//        btnLogin.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//
-//                Toast toast = Toast.makeText(getContext(), "This is login button", Toast.LENGTH_SHORT);
-//                toast.show();
-//            }
-//        });
-//
-//        btnRegister.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(getContext(), RegistrationActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-
