a6047447bf1de2e9875d9dc7e5cd497dd93c5250
SarahSolkar <sarahsolkar123@gmail.com>
2019-09-28 21:39:55 +0530

    Update ModelFood.java

--- app/src/main/java/com/example/sarah/nav/ModelFood.java
+++ app/src/main/java/com/example/sarah/nav/ModelFood.java
@@ -1,26 +1,26 @@
 package com.example.sarah.nav;
 
 public class ModelFood {
-    private String restaurant_name, category, price,imgname, rid;
+    private String restaurant_name, category, price,imgname,rid;
 
-    public String getRid() {return rid; }
     public String getRestaurant_name() { return restaurant_name; }
     public String getCategory() { return category; }
     public String getImgname() { return imgname; }
     public String getPrice() {
         return price;
     }
+    public String getRid(){return rid;}
 
-    public void setPrice(String price) {
-        this.price = price;
-    }
+//    public void setPrice(String price) {
+//        this.price = price;
+//    }
 
-    public ModelFood(String restaurant_name, String category, String imgname,String price) {
+    public ModelFood(String restaurant_name, String category, String imgname,String price, String rid) {
         this.restaurant_name = restaurant_name;
         this.category = category;
         this.imgname = imgname;
         this.price = price;
+        this.rid = rid;
     }
-
 }
 
