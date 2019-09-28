52e0f8f4dd3ca958ddfa11b02e4ac244a516856b
SarahSolkar <sarahsolkar123@gmail.com>
2019-09-28 21:41:04 +0530

    Update HomeFragment.java

--- app/src/main/java/com/example/sarah/nav/HomeFragment.java
+++ app/src/main/java/com/example/sarah/nav/HomeFragment.java
@@ -1,12 +1,14 @@
 package com.example.sarah.nav;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.content.SharedPreferences;
@@ -14,14 +16,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.Volley;
+import com.facebook.shimmer.ShimmerFrameLayout;
 import com.lorentzos.flingswipe.SwipeFlingAdapterView;
 import com.squareup.picasso.Picasso;
 
@@ -29,6 +34,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 
@@ -37,9 +43,18 @@ public class HomeFragment extends Fragment {
     private MyAppAdapter myAppAdapter;
     ArrayList<Data> array;
     SwipeFlingAdapterView flingContainer;
+    String del;
+    String email_session="";
+    SessionManager sessionManager;
+    //ShimmerFrameLayout mShimmerViewContainer;
+
+    //ProgressBar loading;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+       // mProgressBar = getActivity().findViewById(R.id.progressBar);
+        //mProgressBar.setVisibility(View.VISIBLE);
         return inflater.inflate(R.layout.home,container,false);
     }
 
@@ -49,14 +64,34 @@ public class HomeFragment extends Fragment {
 
         array = new ArrayList<>();
         getIp ip = new getIp();
-        String del = ip.getIp();
+        //loading = getActivity().findViewById(R.id.loading_home);
+        //mProgressBar.setVisibility(View.VISIBLE);
+        //mShimmerViewContainer = getActivity().findViewById(R.id.shimmer_view_container);
 
-        //array.add(new Data("http://192.168.0.103:8080/images/nonveg/1082564.jpg", "1", "64", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."));
-        array.add(new Data("Guddu ka Dhaaba", "apple pie", "407267.jpg", "50", "Lorem ipsum dolor sit amet, consectetur adipiscing elit,","1"));
 
-        getUrls();
-        Log.d("new", "initial " + array);
+        sessionManager = new SessionManager(getActivity().getApplicationContext());
+        sessionManager.checkLogin();
+        HashMap<String, String> user = sessionManager.getUserDetails();
+        email_session = user.get(sessionManager.EMAIL);
+        del = ip.getIp();
+
+        array.add(new Data("Guddu ka Dhaaba", "apple pie", "apple pie_68383.jpg", "50", "Lorem ipsum dolor sit amet, consectetur adipiscing elit,","1"));
+
+        getUrls(email_session);
+
         flingContainer = (SwipeFlingAdapterView) getActivity().findViewById(R.id.frame);
+//        refresh_button = getActivity().findViewById(R.id.refresh_button);
+//        refresh_button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+////                Intent r = new Intent(getContext(),MainActivity.class);
+////                startActivity(r);
+//                FragmentTransaction ft = getFragmentManager().beginTransaction();
+//                ft.detach(HomeFragment.this).attach(HomeFragment.this).commit();
+//                myAppAdapter.notifyDataSetChanged();
+//                Toast.makeText(getActivity().getApplicationContext(), "REEEEE", Toast.LENGTH_SHORT).show();
+//            }
+//        });
 
 
         final SwipeRefreshLayout refresh = (SwipeRefreshLayout) getActivity().findViewById(R.id.refresh);
@@ -68,83 +103,85 @@ public class HomeFragment extends Fragment {
                     @Override
                     public void run() {
                         Toast.makeText(getActivity().getApplicationContext(), "REEEEE", Toast.LENGTH_SHORT).show();
-                       myAppAdapter.notifyDataSetChanged();
+                        myAppAdapter.notifyDataSetChanged();
                         refresh.setRefreshing(false);
                     }
-                }, 2000);
+                }, 1000);
             }
         });
 
-        flingContainer.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
-            @Override
-            public void removeFirstObjectInAdapter() {
-                //myAppAdapter.notifyDataSetChanged();
-            }
+                flingContainer.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
+                    @Override
+                    public void removeFirstObjectInAdapter() {
+                        //myAppAdapter.notifyDataSetChanged();
+                    }
 
-            @Override
-            public void onLeftCardExit(Object dataObject) {
-                Data a=array.remove(0);
-                Log.d("array remove", String.valueOf(a));
-                myAppAdapter.notifyDataSetChanged();
-                //Do something on the left!
-                //You also have access to the original object.
-                //If you want to use it just cast it (String) dataObject
-            }
+                    @Override
+                    public void onLeftCardExit(Object dataObject) {
+                        Data a=array.remove(0);
+                        Log.d("array remove", String.valueOf(a));
+                        myAppAdapter.notifyDataSetChanged();
+                        //Do something on the left!
+                    }
 
-            @Override
-            public void onRightCardExit(Object dataObject) {
+                    @Override
+                    public void onRightCardExit(Object dataObject) {
 
-                Data a=array.remove(0);
-                Log.d("array remove", String.valueOf(a));
+                        Data a=array.remove(0);
+                        Log.d("array remove", String.valueOf(a));
 
-                String restaurant_name=a.getRestaurant_name();
-                String category = a.getCategory();
-                String imgname = a.getImgname();
-                String rid = a.getRid();
-                //String desc = a.getDescription();
-
-                Log.d("right card rest name",restaurant_name);
-                Log.d("right card category ",category);
-                Log.d("right card img ",imgname);
-                Log.d("right card rid ",rid);
-                //Log.d("right card desc",desc);
-
-
-
-
-
-                rightSwipe(restaurant_name,category,imgname,rid);
+                        String restaurant_name=a.getRestaurant_name();
+                        String category = a.getCategory();
+                        String imgname = a.getImgname();
+                        String rid = a.getRid();
+                        rightSwipe(restaurant_name,category,imgname,rid);
 
                 myAppAdapter.notifyDataSetChanged();
                 Toast.makeText(getActivity().getApplicationContext(),"RiGHT",Toast.LENGTH_SHORT).show();
-
             }
 
             @Override
             public void onAdapterAboutToEmpty(int itemsInAdapter) {
-                getUrls();
+                getUrls(email_session);
             }
 
             @Override
             public void onScroll(float scrollProgressPercent) {
-
                 View view = flingContainer.getSelectedView();
                 view.findViewById(R.id.background).setAlpha(0);
                 view.findViewById(R.id.item_swipe_right_indicator).setAlpha(scrollProgressPercent < 0 ? -scrollProgressPercent : 0);
                 view.findViewById(R.id.item_swipe_left_indicator).setAlpha(scrollProgressPercent > 0 ? scrollProgressPercent : 0);
             }
         });
+        refresh.setEnabled(true);
 
         flingContainer.setOnItemClickListener(new SwipeFlingAdapterView.OnItemClickListener() {
             @Override
             public void onItemClicked(int itemPosition, Object dataObject) {
-
                 View view = flingContainer.getSelectedView();
                 view.findViewById(R.id.background).setAlpha(0);
                 myAppAdapter.notifyDataSetChanged();
             }
         });
     }
+
+//    @Override
+//    public void onResume() {
+//        super.onResume();
+//        mShimmerViewContainer.startShimmer();
+//    }
+
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        mShimmerViewContainer.startShimmer();
+//    }
+//
+//    @Override
+//    public void onPause() {
+//        super.onPause();
+//        mShimmerViewContainer.stopShimmer();
+//    }
 
     public static class ViewHolder {
         public FrameLayout background;
@@ -153,8 +190,6 @@ public class HomeFragment extends Fragment {
         }
 
     public class MyAppAdapter extends BaseAdapter {
-
-
         List<Data> parkingList;
         public Context context;
 
@@ -182,8 +217,6 @@ public class HomeFragment extends Fragment {
         public View getView(final int position, View convertView, ViewGroup parent) {
 
             View rowView = convertView;
-
-
             ViewHolder viewHolder;
             if (rowView == null) {
 
@@ -199,33 +232,34 @@ public class HomeFragment extends Fragment {
             } else {
                 viewHolder = (ViewHolder) convertView.getTag();
             }
-            //viewHolder.DataText.setText(parkingList.get(position).getDescription());
-            viewHolder.DataText.setText(parkingList.get(position).getDescription() + "" +parkingList.get(position).getPrice()+""+parkingList.get(position).getRestaurant_name());
+
+            viewHolder.DataText.setText("Restaurant Name: "+parkingList.get(position).getRestaurant_name() + "\n\n" +"Food Name: "+parkingList.get(position).getCategory()+"\n\n"+"Description:\n"+parkingList.get(position).getDescription()+"\n\n\n"+"Price: ₹"+parkingList.get(position).getPrice());
 
             //String urlImage = parkingList.get(position).getImagePath();
-            String urlImage = parkingList.get(position).getImgname();
+            String urlImage = del+":8080/images/"+parkingList.get(position).getRid()+"/"+parkingList.get(position).getCategory()+"/"+parkingList.get(position).getImgname();
+            //Log.d("url","url "+urlImage);
 
-            Log.d("url","url "+urlImage);
-            //Glide.with(getApplicationContext()).load(urlImage).into(viewHolder.cardImage);
             Picasso.get().load(urlImage).error(R.drawable.ic_launcher_background).fit().centerCrop().into(viewHolder.cardImage);
             return rowView;
         }
     }// end of myAdapter
 
 
-    public void getUrls(){
+    public void getUrls(String email){
+
+
+//        myAppAdapter.notifyDataSetChanged();
 
         for(int i=1;i<=1;i++) {
             getIp ip = new getIp();
             String del = ip.getIp();
 
             RequestQueue requestQueue = Volley.newRequestQueue(Objects.requireNonNull(getActivity()).getApplicationContext());
-            //String URL = "http://192.168.0.103:8080/addSwipe";
             String URL = ""+del+":8080/addSwipe";
 
             final JSONObject jsonObject = new JSONObject();
             try {
-                jsonObject.put("test", "NULL");
+                jsonObject.put("email", email);
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -236,12 +270,10 @@ public class HomeFragment extends Fragment {
                 public void onSuccessResponse(String result) {
                     System.out.print("Bool" + result);
                     if (result != null) {
-                        //String[] load = result.split(",");
-                        //String ip= null, index1 = null, index2 = null, description = null;
-
                         String ip, restaurant_name = null, category = null, price = null,description = null, rid=null, imgname = null;
-
                         try {
+                            Log.d("result    ",""+result);
+
                             JSONObject jsonObject1 = new JSONObject(result);
                             ip = jsonObject1.getString("ip");
                             restaurant_name = jsonObject1.getString("restaurant_name");
@@ -250,6 +282,8 @@ public class HomeFragment extends Fragment {
                             price = jsonObject1.getString("price");
                             rid = jsonObject1.getString("rid");
                             description = jsonObject1.getString("description");
+                            Log.d("descooo",""+jsonObject1);
+
 
                             System.out.println("ip : "+ip+" restaurant_name : "+restaurant_name+" category = "+category+" price = "+price+" rid = "+rid );
 
@@ -268,17 +302,14 @@ public class HomeFragment extends Fragment {
 
                         array.add(new Data(restaurant_name,category,imgname,price,description,rid));
                         Log.d("array",""+array);
-
-
-                        //Log.d("Result", "Result is " + result +" and "+ array);
                         myAppAdapter = new MyAppAdapter(array, getActivity());
                         flingContainer.setAdapter(myAppAdapter);
+                        //mProgressBar.setVisibility(View.INVISIBLE);
 
                     } else {
                         Toast.makeText(getActivity().getApplicationContext(), "Something went wrong", Toast.LENGTH_SHORT).show();
                     }
                 }
-
                 @Override
                 public void onErrorResponse(VolleyError error) {
                     Toast toast = Toast.makeText(getActivity().getApplicationContext(),
@@ -286,9 +317,18 @@ public class HomeFragment extends Fragment {
                             Toast.LENGTH_LONG);
                     toast.show();
                 }
-
             });
+
         }
+
+//        Handler handler = new Handler();
+//        handler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                loading.setVisibility(View.GONE);
+//                myAppAdapter.notifyDataSetChanged();
+//            }
+//        },5000);
 
     }//end of getUrls()
 
@@ -334,29 +374,7 @@ public class HomeFragment extends Fragment {
             public void onSuccessResponse(String result) {
                 //System.out.print("Bool" + result);
                 if (result.equals("1")) {
-//                    String ip= null, restaurant_name = null, category = null, imgname = null, rid = null,description = null;
-//                    try {
-//                        JSONObject jsonObject1 = new JSONObject(result);
-//                        ip = jsonObject1.getString("ip");
-//                        restaurant_name = jsonObject1.getString("restaurant_name");
-//                        category = jsonObject1.getString("category");
-//                        imgname = jsonObject1.getString("image");
-//                        rid = jsonObject1.getString("rid");
-//                        description = jsonObject1.getString("description");
-//
-//                        System.out.println("ip :"+ip+ "restaurant_name : "+restaurant_name+"category = "+category+"image name = "+imgname+"rid = "+rid );
-//
-//                    } catch (JSONException e) {
-//                        e.printStackTrace();
-//                    }
-//
-//                    array.add(new Data(restaurant_name,category,imgname,rid,description));
-//                    Log.d("Result", "Result is " + result +" and "+ array);
-//                    myAppAdapter = new MyAppAdapter(array, getActivity());
-//                    flingContainer.setAdapter(myAppAdapter);
                     Toast.makeText(getActivity().getApplicationContext(), "Inserted into WishList", Toast.LENGTH_SHORT).show();
-
-
                 } else {
                     Toast.makeText(getActivity().getApplicationContext(), "Something went wrong", Toast.LENGTH_SHORT).show();
                 }
@@ -369,11 +387,6 @@ public class HomeFragment extends Fragment {
                         Toast.LENGTH_LONG);
                 toast.show();
             }
-
         });
-
-
-
-
     }
 }
