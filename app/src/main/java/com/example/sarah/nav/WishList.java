0911381c60f84661845265c7985f9aeeeed28a2f
SarahSolkar <sarahsolkar123@gmail.com>
2019-09-28 21:37:42 +0530

    Update Wishlist.java

--- app/src/main/java/com/example/sarah/nav/WishList.java
+++ app/src/main/java/com/example/sarah/nav/WishList.java
@@ -1,5 +1,6 @@
 package com.example.sarah.nav;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,24 +8,31 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.Volley;
+import com.facebook.shimmer.ShimmerFrameLayout;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class WishList extends AppCompatActivity {
     FoodAdapter foodAdapter;
-    ArrayList<ModelFood> foodList;
-    String email;
-    SharedPreferences pref;
+    ArrayList<Data> foodList;
+    String email="";
+    SessionManager sessionManager;
     RecyclerView recyclerView;
+    //shimmer
+    ProgressBar loading;
+    ShimmerFrameLayout mShimmerViewContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,23 +40,21 @@ public class WishList extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.wishlist);
-
-        pref = getSharedPreferences("email",MODE_PRIVATE);
-        //result.setText("Hello, "+prf.getString("username",null));
-        Toast.makeText(WishList.this,"Hello "+pref,Toast.LENGTH_SHORT).show();
-
-        //rec email from main activity
-        Bundle bundle = getIntent().getExtras();
-        assert bundle != null;
-        email=bundle.getString("params");
-        Log.d("prams",email);
-
-
-        //Adding back button
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        foodList = new ArrayList<>();
+        assert getSupportActionBar() != null;
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        sessionManager = new SessionManager(getApplicationContext());
+        sessionManager.checkLogin();
+        HashMap<String, String> user = sessionManager.getUserDetails();
+        email = user.get(sessionManager.EMAIL);
+
         recyclerView = findViewById(R.id.rv);
+        //loading=findViewById(R.id.loading);
+
+
+        //shimmer
+        mShimmerViewContainer = findViewById(R.id.shimmer_view_container);
 
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         RecyclerView.LayoutManager rvlayoutmanager = linearLayoutManager;
@@ -56,33 +62,42 @@ public class WishList extends AppCompatActivity {
 
         getData();
     }
-///Adding back button
+
+    //shimmer
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        int id=item.getItemId();
-        if(id==android.R.id.home){
-            this.finish();
-        }
-
-        return super.onOptionsItemSelected(item);
+    protected void onResume() {
+        super.onResume();
+        mShimmerViewContainer.startShimmer();
     }
-/////////////////////////////
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mShimmerViewContainer.stopShimmer();
+    }
+//end shimmer
+    @Override
+    public boolean onSupportNavigateUp(){
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+        finish();
+        return true;
+    }
+
+    /////////////////////////////////////////////////////////////////////
     public void getData(){
         getIp ip = new getIp();
         String del = ip.getIp();
+
         RequestQueue requestQueue = Volley.newRequestQueue(WishList.this);
-        String URL = ""+del+":8080/getLoc";
+        String URL = ""+del+":8080/getWishlist";
 
         JSONObject jsonObject = new JSONObject();
         try {
-            //jsonObject.put("test", "null");
             jsonObject.put("email", email);
-
         } catch (Exception e) {
             e.printStackTrace();
         }
-
         final String requestBody = jsonObject.toString();
         Log.d("str", "str is" + requestBody);
 
@@ -90,10 +105,6 @@ public class WishList extends AppCompatActivity {
             @Override
             public void onSuccessResponse(String result) {
                 Log.d("result is ", "" + result);
-
-                result = result.replaceAll("\'", "");
-
-
                 if (result != null) {
                     try {
                         JSONArray jsonArray = new JSONArray(result);
@@ -103,44 +114,35 @@ public class WishList extends AppCompatActivity {
                         for (i = 0; i < jsonArray.length(); i++) {
                             JSONObject jsonObject1 = jsonArray.getJSONObject(i);
 
-                            String loc = null;
+                            String restaurant_name = null,category = null, imgname = null, price = null,rid = null;
+                            String description = "Lorem ipsum dolor sit amet, proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
                             try {
-                                loc = jsonObject1.getString("location");
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                            String name = null;
-                            try {
-                                name = jsonObject1.getString("name");
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                            String price = null;
-                            try {
+                                restaurant_name = jsonObject1.getString("restaurant_name");
+                                category = jsonObject1.getString("category");
+                                imgname = jsonObject1.getString("image");
                                 price = jsonObject1.getString("price");
-                            } catch (JSONException e) {
+                                rid = jsonObject1.getString("rid");
+                                //description = jsonObject1.getString("description");
+                            }
+                            catch (JSONException e) {
                                 e.printStackTrace();
                             }
-                            Log.d("loc", "" + loc);
-                            Log.d("name", "" + name);
-                            Log.d("price", "" + price);
-                            foodList.add(new ModelFood(loc, name, price));
+                            foodList.add(new Data(restaurant_name, category, imgname,price, description,rid));
                         }
 
                         Log.d("foodlist", "" + foodList);
                         final FoodAdapter foodAdapter = new FoodAdapter(WishList.this, foodList);
                         recyclerView.setAdapter(foodAdapter);
+                        mShimmerViewContainer.stopShimmer();
+                        mShimmerViewContainer.setVisibility(View.GONE);
 
                         foodAdapter.setOnItemClickListener(new FoodAdapter.OnItemClickListener() {
                             public void onItemClick(int position) {
-                                ModelFood item = foodList.get(position);
-                                Log.d("Andar ka maal", ""+item);
-                                Log.d("maal ka index 1", ""+item.getIndex1());
-                                Log.d("maal ka index 2", ""+item.getIndex2());
-                                Log.d("maal ka price", ""+item.getPrice());
-                                sendData(item.getIndex1(), item.getIndex2(), item.getPrice());
+                                Data item = foodList.get(position);
+                                sendData(item.getRestaurant_name(), item.getCategory(), item.getImgname(), item.getRid());
                                 foodList.remove(position);
                                 foodAdapter.notifyItemRemoved(position);
+
                                 Toast.makeText(WishList.this, "Sent to cart Successfully", Toast.LENGTH_SHORT).show();
 
                             }
@@ -155,23 +157,30 @@ public class WishList extends AppCompatActivity {
             }
             @Override
             public void onErrorResponse(VolleyError error) {
-
-                Log.d("error: ", "hagg diya");
+                Log.d("error: ", "Volley needs attention");
             }
 
         });
     }
-    public void sendData(String index1, String index2, String price){
+
+    //end of getUrls/////////////////////////////////////////////////////////////
+
+
+    public void sendData(String restaurant_name, String category, String imgname, String rid){
 
         RequestQueue requestQueue = Volley.newRequestQueue(WishList.this);
-
+        getIp ip = new getIp();
+        String del = ip.getIp();
+        String URL = ""+del+":8080/order";
 
         JSONObject jsonObject = new JSONObject();
         try {
-            jsonObject.put("index2", ""+index1);
-            jsonObject.put("name", ""+index2);
-            //jsonObject.put("price", ""+price);
-            jsonObject.put("email","leeaanair@gmail.com");
+            jsonObject.put("restaurant_name", ""+restaurant_name);
+            jsonObject.put("category", ""+category);
+            jsonObject.put("email",email);
+            jsonObject.put("image",""+imgname);
+            jsonObject.put("rid",""+rid);
+
             Log.d("jsonobject", ""+jsonObject);
 
         } catch (Exception e) {
@@ -179,22 +188,23 @@ public class WishList extends AppCompatActivity {
         }
 
         final String requestBody = jsonObject.toString();
-        Log.d("str", "str is" + requestBody);
-        getIp ip = new getIp();
-        String del = ip.getIp();
-        Log.d("ip",del);
-        String URL = ""+del+":8080/sendData";
         ConnectionManager.sendData(requestBody, requestQueue, URL, new ConnectionManager.VolleyCallback() {
             @Override
             public void onSuccessResponse(String result) {
-                Log.d("data hua send =", "" + result);
+                Log.d("Data sent =", "" + result);
             }
             @Override
             public void onErrorResponse(VolleyError error) {
-
-                Log.d("error: ", "data send main problem");
+                Log.d("error: ", "Volley needs attention");
             }
 
         });
+    }
+/////////////////////////////////
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
     }
 }
