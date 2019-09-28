9566ac4a73213b7498071d36ea1a22115350afb1
SarahSolkar <35928732+SarahSolkar@users.noreply.github.com>
2019-09-28 09:32:05 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -7,7 +7,7 @@ import sqlite3
 import hashlib
 import re
 
-ip = "http://192.168.0.103:8080"
+ip = "http://your_ip_address:8080"
 description = "Lorem ipsum dolor sit amet, proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 app = Flask(__name__, static_url_path='' )
 
