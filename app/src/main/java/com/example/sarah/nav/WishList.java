243c0c1d7e8f7e393017990a6ecf9c6112d0ed10
SarahSolkar <sarahsolkar123@gmail.com>
2019-09-30 22:11:01 +0530

    Add refresh

--- app/src/main/java/com/example/sarah/nav/WishList.java
+++ app/src/main/java/com/example/sarah/nav/WishList.java
@@ -81,6 +81,7 @@ public class WishList extends AppCompatActivity {
         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
         startActivity(intent);
         finish();
+        Toast.makeText(this, "Refresh to Load Items", Toast.LENGTH_SHORT).show();
         return true;
     }
 
